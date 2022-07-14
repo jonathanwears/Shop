84826f2dfb2f382d419d8e1768aa36e362f254f5
Jona <jonathan@pop-os.localdomain>
2022-07-14 16:38:20 +0200

    initial commit

---
+++ src/pages/Products.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+function Products() {
+  let a = ' string';
+    a= 'banana';
+
+    return (
+      <BrowserRouter>
+      hello
+        // product list
+        // product information
+      </BrowserRouter>
+    );
+}
+
+export default Products;
